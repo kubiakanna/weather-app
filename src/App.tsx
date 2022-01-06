37b88be780aa6f347ecd85039cf2aaa1c9c6e29b
annakubiak <89855163+kubiakanna@users.noreply.github.com>
2022-01-06 17:47:08 +0100

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,25 @@ function App() {
       if(typeof weather.main.temp !== 'undefined') {
         const temperature = Math.round(weather.main.temp);
         if(temperature < 15) {
-            return 'app app__cold';
+            return 'app__cold';
         }
-        return 'app app__warm';
+        return 'app__warm';
       }
-      return 'app';
   }
 
   const mainDisplayClasses = () => {
     if(typeof weather.main.temp !== 'undefined') {
       const temperature = Math.round(weather.main.temp);
       if(temperature < 15) {
-          return 'main-display main-display__cold';
+          return 'main-display__cold';
       }
-      return 'main-display main-display__warm';
+      return 'main-display__warm';
     }
-    return 'main-display';
   }
 
   return (
-    <div className={appClasses()}>
-      <div className={mainDisplayClasses()}>
+    <div className={'app ' + appClasses()}>
+      <div className={'main-display ' + mainDisplayClasses()}>
         <SearchBar city={ city } setCity={ setCity } weather={ weather } setWeather={ setWeather } />
         <WeatherDisplay weather={ weather } />
       </div>
